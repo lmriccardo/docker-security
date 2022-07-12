8f5661dbc61af47e03ccd68ce506db1983306cad
Riccardo La Marca <absintio098@gmail.com>
2022-07-12 22:20:29 +0200

    Update docker-scan-download.sh

--- src/securing_docker/docker-scan-download.sh
+++ src/securing_docker/docker-scan-download.sh
@@ -10,5 +10,5 @@
 cd /home
 wget https://download.docker.com/linux/ubuntu/dists/hirsute/pool/stable/amd64/docker-scan-plugin_0.17.0~ubuntu-hirsute_amd64.deb
 sudo dpkg -i docker-scan-plugin_0.17.0\~ubuntu-hirsute_amd64.deb
-rm docker-scan-plugin_0.7.0\~ubuntu-focal_amd64.deb
+rm docker-scan-plugin_0.17.0\~ubuntu-hirsute_amd64.deb
 exit 0
